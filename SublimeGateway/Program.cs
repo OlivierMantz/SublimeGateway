1741a1237c98d7faceebf93d1289705fe9d62daf
OlivierMantz <OlivierMantz>
2023-11-23 14:52:41 +0100

    Add project files.

---
+++ SublimeGateway/Program.cs
@@ -0,0 +1,51 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Cache.CacheManager;
+
+namespace SublimeGateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddOcelot(builder.Configuration)
+            .AddCacheManager(x =>
+            {
+                x.WithDictionaryHandle();
+            });
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.UseOcelot().Wait();
+
+            app.Run();
+        }
+
+    }
+}
